0aaba391dcbbe20d747c4c9f4a17836c0dd9f15f
Amir Reza Doost Mohammadi <amirrezadm81@gmail.com>
2019-06-29 04:15:11 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World + React with parcel</h1>
+            <p>this tutorial in virgoole.io</p>
+        </div>
+    )
+};
+
+export default App;
